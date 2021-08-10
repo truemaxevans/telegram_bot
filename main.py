8acf81578b77cf5d20f40cf017137015aeba7450
truemaxevans <daorodney@gmail.com>
2021-08-10 12:14:52 +0300

    Added ReplyKeyBoardMarkup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 import telebot
+from telebot import types
+import random
 
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
@@ -11,9 +13,15 @@ bot = telebot.TeleBot(API_KEY)
 def send_welcome(message):
     bot.reply_to(message, "Hi, how are you doing?")
 
+#keyboard
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+item1 = types.KeyboardButton('Available commands')
+item2 = types.KeyboardButton('Exit')
+markup.add(item1, item2)
 @bot.message_handler(commands=['help', 'gethelp'])
 
 def help(message):
-    bot.send_message(message.chat.id, "How I can help you?")
+    bot.send_message(message.chat.id, "How I can help you?", reply_markup=markup)
 
 bot.polling()
+
